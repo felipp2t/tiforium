d2cfdbc5cd11e719497d8706fd8b125fd0855f09
Felipe Rossetto <feliperossettodasilva123@gmail.com>
2024-10-20 07:54:27 -0300

    feat: add match entity

---
+++ src/app/entities/match.ts
@@ -0,0 +1,25 @@
+import type { UUID } from 'node:crypto';
+import type { Round } from './round';
+
+interface MatchProps {
+  id: UUID;
+  rounds: Round[];
+}
+
+export class Match {
+  private props: MatchProps;
+
+  constructor(props: MatchProps) {
+    this.props = {
+      ...props,
+    };
+  }
+
+  get id(): string {
+    return this.props.id;
+  }
+
+  get rounds(): Round[] {
+    return this.props.rounds;
+  }
+}
